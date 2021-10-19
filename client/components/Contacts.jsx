ade6f96dcef291c0552a9cbb12a9f6e2daa1abab
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-19 15:31:00 +1300

    created two more components to handle conversations / contacts in Nav

---
+++ client/components/Contacts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contacts = () => {
+
+   return(
+      <div>
+         Contacts
+      </div>
+   )
+}
+
+export default Contacts
--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Tab, Nav } from 'react-bootstrap'
 
+//components
+import Conversations from './Conversations'
+import Contacts from './Contacts'
 
 const Sidebar = ({id}) => {
 
+   const conversationsKey = 'Conversations'
+   const contactsKey = 'Contacts'
+
+   const [activeKey, setActiveKey] = useState(conversationsKey)
+
    return(
       <div className="sidebar-container">
-         <Tab.Container>
-            <Nav>
+         <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
+            <Nav variant="tabs" className="justify-content-center">
                <Nav.Item>
-                  <Nav.Link eventkey='Conversations'>Conversations</Nav.Link>
+                  <Nav.Link eventkey={conversationsKey}>Conversations</Nav.Link>
                </Nav.Item>
                <Nav.Item>
-                  <Nav.Link eventkey='Contacts'>Contacts</Nav.Link>
+                  <Nav.Link eventkey={contactsKey}>Contacts</Nav.Link>
                </Nav.Item>
             </Nav>
          </Tab.Container>
