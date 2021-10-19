6030808d4e84e976c6773ad0e477199347e48029
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-19 15:21:09 +1300

    basic mark up for sidebar Nav

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Tab, Nav } from 'react-bootstrap'
 
 
 const Sidebar = ({id}) => {
 
    return(
-      <>
-         Sidebar working
-      </>
+      <div className="sidebar-container">
+         <Tab.Container>
+            <Nav>
+               <Nav.Item>
+                  <Nav.Link eventkey='Conversations'>Conversations</Nav.Link>
+               </Nav.Item>
+               <Nav.Item>
+                  <Nav.Link eventkey='Contacts'>Contacts</Nav.Link>
+               </Nav.Item>
+            </Nav>
+         </Tab.Container>
+      </div>
    )
 }
 
