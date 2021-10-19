abe2212e5de62b2d9f79d5519d317540ccff0f4c
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-19 14:22:50 +1300

    created handleSubmit for form

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // components
 import Login from './Login'
 
 const App = () => {
+
+   const [id, setId] = useState(null)
+
    return(
       <>
-         <Login />
+         <Login setId={setId}/>
       </>
    )
 }
--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,13 +1,19 @@
 import React, { useRef } from 'react';
 import { Container, Form, Button } from 'react-bootstrap'
 
-const Login = () => {
+const Login = ({setId}) => {
 
    const idRef = useRef()
 
+   // Handle form submit & update ref with input value
+   const handleSubmit = (e) => {
+      e.preventDefault()
+      setId(idRef.current.value)
+   }
+
    return(
       <Container className='align-items-center d-flex' style={{height: '100vh'}}>
-         <Form className='w-100'>
+         <Form className='w-100' onSubmit={handleSubmit}>
             <Form.Group >
                <Form.Label>Enter your Id</Form.Label>
                <Form.Control type="text" ref={idRef} required></Form.Control>
