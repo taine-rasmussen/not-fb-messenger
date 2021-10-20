5b3562407fd36b0706ac0d38e5a4480707f6eb18
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-20 21:54:41 +1300

    auto scroll happens on message received

--- client/components/OpenConversation.jsx
+++ client/components/OpenConversation.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { Form, InputGroup, Button } from 'react-bootstrap'
 import { useConversations } from '../Context/ConversationsProvider';
 
@@ -6,9 +6,16 @@ import { useConversations } from '../Context/ConversationsProvider';
 const OpenConversation = () => {
 
    const { sendMessage, selectedConversation } = useConversations()
-
    const [text, setText] = useState('')
 
+    const setRef = useCallback(node => {
+    if (node) {
+      node.scrollIntoView({ smooth: true })
+    }
+  }, [])
+
+
+   
    const handleSubmit = (e) => {
       e.preventDefault()
       sendMessage(selectedConversation.recipients.map(r => r.id),text)
@@ -18,12 +25,16 @@ const OpenConversation = () => {
    return(
       <div className="d-flex flex-column flex-grow-1">
          <div className="flex-grow-1 overflow-auto">
-            <div className="h-100 d-flex flex-column align-items-start justify-content-end px-3 ">
+            <div className="d-flex flex-column align-items-start justify-content-end px-3 ">
                {selectedConversation.messages.map((message, index) => {
+                  const lastMessage = selectedConversation.messages.length - 1 === index
                   return(
-                     <div className='my-1 d-flex flex-column' key={index}>
-                        <div 
-                        className={`rounded px-2 py-1 ${message.fromMe ? 'bg-primary text-white' : 'border'}`}>
+                     <div 
+                        ref={lastMessage ? setRef : null}
+                        className={`my-1 d-flex flex-column ${message.fromMe ? 'align-self-end align-items-end' : 'align-items-start'}`}
+                        key={index}
+                     >
+                        <div className={`rounded px-2 py-1 ${message.fromMe ? 'bg-primary text-white' : 'border'}`}>
                            {message.text}
                         </div>
                         <div className={`text-muted small ${message.fromMe ? 'text-right' : ''}`}>
@@ -34,7 +45,7 @@ const OpenConversation = () => {
                })}
 
             </div>
-
+ 
          </div> 
             <Form onSubmit={handleSubmit}>
                <Form.Group className="m-2">
